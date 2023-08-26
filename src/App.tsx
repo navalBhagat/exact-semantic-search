ffb9e908945b1dd6e4651196536df116fbd49f7d
Naval Tapan Bhagat <59922532+navalBhagat@users.noreply.github.com>
2023-08-26 22:08:46 +0200

    Added a title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { SearchEngine } from "./app/SearchEngine";
+import { Title } from "./app/Title";
 
 function App() {
   return (
     <div className="app">
+      <Title />
       <SearchEngine />
     </div>
   );
---
+++ src/app/Title.tsx
@@ -0,0 +1,7 @@
+export const Title = () => {
+  return (
+    <div className="title">
+      Let's find your <span className="exact">Exact</span> answer
+    </div>
+  );
+};
